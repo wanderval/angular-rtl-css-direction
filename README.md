b18a9131d4a37c330f52f630319356d5955102cd
wanderval <val.ozzy@hotmail.com>
2019-09-12 23:25:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rtl-css-direction
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rtl-css-direction)
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
